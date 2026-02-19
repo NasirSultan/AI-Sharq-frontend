3a8048fd93140455ea129fa66ce885ccb5f3b9e7
NasirSultan <rainasirsultan123@gmail.com>
2026-02-20 02:13:19 +0500

    Update API baseURL to new backend endpoint

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,9 @@
 import axios from "axios"
 
 const api = axios.create({
-  
-    baseURL: "https://apiconnect.sharqforum.org",
+ 
+      baseURL: " https://al-sharq-conference-backend.onrender.com",
+    // baseURL: "https://apiconnect.sharqforum.org",
   // baseURL: "http://138.68.104.206:3000", 
     // baseURL: "http://localhost:5000",// your backend
   withCredentials: true,            // send cookies if needed
